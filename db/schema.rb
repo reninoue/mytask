4e6717c28e6b1d3d79d7aeaba3f9640fefef76f3
reninoue <71805701+reninoue@users.noreply.github.com>
2020-11-10 18:31:47 +0900

    モデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_10_085651) do
+
+ActiveRecord::Schema.define(version: 2020_11_10_091816) do
+
+  create_table "lists", force: :cascade do |t|
+    t.string "title", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_lists_on_user_id"
+  end
+
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,5 +34,9 @@ ActiveRecord::Schema.define(version: 2020_11_10_085651) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+
+
+  add_foreign_key "lists", "users"
 
 end
